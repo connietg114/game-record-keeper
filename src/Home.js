e702e937a5840a6bfc2baa59b4caae586e10cc6d
Connie Tang <kourtneytg@gmail.com>
2020-08-18 17:10:04 +1200

    remove previous Home page content

--- src/Home.js
+++ src/Home.js
@@ -3,51 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Home(){
+    // var games = [
+    //     {ID: '100',
+    //     name: 'Catan',
+    //     status: 'dontknow',
+    //     date: '2020-03-03',
+    //     noOfPlayers: 10,
+    //     tournamentID: '200'},
+
+    //     {ID: '110',
+    //     name: 'Monopoly',
+    //     status: 'null',
+    //     date: '2020-03-06',
+    //     noOfPlayers: 10,
+    //     tournamentID: '200'},
+
+    //     {ID: '111',
+    //     name: 'Go',
+    //     status: 'dontknow',
+    //     date: '2020-02-10',
+    //     noOfPlayers: 10,
+    //     tournamentID: '200'},
+
+    //     {ID: '100',
+    //     name: 'Connect Four',
+    //     status: 'dontknow',
+    //     date: '2020-02-20',
+    //     noOfPlayers: 10,
+    //     tournamentID: '200'},
+
+    //     {ID: '100',
+    //     name: 'Chess',
+    //     status: 'dontknow',
+    //     date: '2020-10-01',
+    //     noOfPlayers: 10,
+    //     tournamentID: '200'},
+        
+    // ];
+    // for (var i = 0; i<game.length;i++){
+    //     console.log(game[i].name);
+    // }
+
+
     
-    const [players, setPlayers]=useState([]);
-    const[text, setText] = useState('');
-
-    var addPlayer = () => {
-        if (text === '') {
-            alert('No name');
-            return;
-        }
-        var playerList = [];
-
-        for(var i = 0; i < players.length; i++) {
-            playerList.push(players[i]);
-        }
-        playerList.push(text);
-        setPlayers(playerList);
-        setText('');
-    }
-   
-    //for remove item from players array
    
     return(
         <div>
             <h1>Home</h1>
             <hr></hr>
-            <input name="newPlayerName" value={text} onChange={e => setText(e.target.value)}></input>
-            <button onClick={addPlayer}>Add Player</button>
             
-            <h3>Form a Table below:</h3>
-            <table>
-            
-            {
-                players.map(p => (
-
-                <tr>
-                    <td>{p}</td>
-                    <td>
-                        <button id='playerName'>Remove</button>
-                                   
-                    </td>
-                </tr>))
-                
-            }
-        
-            </table>
             </div>
     );
 }
--- src/Theresults.js
+++ src/Theresults.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './Theresults.css';
 
 function Theresults(){
+    const [players, setPlayers]=useState([]);
+    const[text, setText] = useState('');
+
+    var addPlayer = () => {
+        if (text === '') {
+            alert('No name');
+            return;
+        }
+        var playerList = [];
+
+        for(var i = 0; i < players.length; i++) {
+            playerList.push(players[i]);
+        }
+        playerList.push(text);
+        setPlayers(playerList);
+        setText('');
+    }
+   
+    //for remove item from players array
     return(
         <div>
-            <h1>The Results</h1>
+            <h1>The Results (draft content)</h1>
             <hr></hr>
+            <input name="newPlayerName" value={text} onChange={e => setText(e.target.value)}></input>
+            <button onClick={addPlayer}>Add Player</button>
+            
+            <h3>Form a Table below:</h3>
+            <table>
+            
+            {
+                players.map(p => (
+
+                <tr>
+                    <td>{p}</td>
+                    <td>
+                        <button id='playerName'>Remove</button>
+                                   
+                    </td>
+                </tr>))
+                
+            }
+        
+            </table>
         </div>
+        
     );
 }
 export default Theresults;
