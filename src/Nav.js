14720e92999f10ca17cb7efe02d917b6cff53040
Connie Tang <kourtneytg@gmail.com>
2020-09-08 23:34:33 +1200

    no message

--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Nav.css';
 import {Link} from 'react-router-dom';
+import { Button, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 function Nav(){
     const style ={
@@ -12,6 +13,25 @@ function Nav(){
         width: '30px'
     };
     return(
+        // <Navbar bg="light" expand="lg">
+        // <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        // <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        // <Navbar.Collapse id="basic-navbar-nav">
+        //     <Nav className="mr-auto">
+        //     <Nav.Link href="#home">Home</Nav.Link>
+        //     <Nav.Link href="#link">Link</Nav.Link>
+        //     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        //         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        //         <NavDropdown.Divider />
+        //         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        //     </NavDropdown>
+        //     </Nav>
+           
+        // </Navbar.Collapse>
+        // </Navbar>
+
         <nav className="topnav">
         <Link style={style} to='/'><img style = {imgStyle} src='./logo192.png' ></img> </Link>
         <Link style={style} to='/'className="active">Home</Link>
@@ -19,12 +39,8 @@ function Nav(){
         <Link style={style} to='/matches'>Matches</Link>
         <Link style={style} to='/games'>Games</Link>
         <Link style={style} to='/players'>Players</Link>
-        
-        <Link style={style} to='/about'>About</Link>
-        
-        
-        
-    </nav>
+        <Link style={style} to='/about'>About</Link>    
+    </nav> 
     );
 }
 export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Row from './createTable';
-import _ from 'lodash';
-import Nav from './Nav.js';
-import Home from './Home.js';
-import About from './About.js';
+import App from './App.js';
 
-import Theresults from './Theresults.js';
-import Matches from './matches.js';
-import Players from './players.js';
-import GameDetail from './GameDetail.js';
-import Tournaments from './Tournaments.js';
-import Games from './Games.js';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import EnhancedTable from './draft';
 
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
 // import { ReactComponent } from '*.svg';
 
 
-function App(props){
-    
-    const [config, setConfig] = useState({});
-
-    fetch('/config.json', {
-        method: 'GET', 
-        headers: {'Content-Type': 'application/json',}
-    })
-    .then(response => response.json())
-    .then(data => setConfig(data)); 
-
-    return(
-        <Router>
-        <div>           
-            <Nav/>
-            <Switch> {/* after detect '/', then stop - Home */}
-                <Route path ='/' exact render={routeProps => (<Home {...routeProps} config={config}/>)}/> {/*put exact so that / works for Home only*/}
-                <Route path='/tournaments' component={Tournaments}/>
-                <Route path='/matches' component={Matches}/>
-                <Route path='/games' component={Games}/>
-                <Route path='/players' component={Players}/>
-                <Route path='/about' component={About}/>
-                <Route path='/theresults' component={Theresults}/>
-                <Route path='/gamedetail' render={routeProps => (<GameDetail {...routeProps} config={config}/>)}/>
-                <Route path='/draft' component={EnhancedTable}/>
-            </Switch>
-            </div> 
-        </Router>                  
-    );   
-}
-// const Home = () => (
-//     <div>
-//         <h1>Home</h1>
-//     </div>
-// );
-
-
-export default App;
 
 ReactDOM.render( <App/>, document.getElementById('root'));
