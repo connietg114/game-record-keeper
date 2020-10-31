5181e91b18d05847a2a2523f81959d9d9dcbf5d8
TracyFei <tracyfeiguiyu7@gmail.com>
2020-11-01 00:23:27 +1300

    About page

--- src/About.js
+++ src/About.js
@@ -34,69 +34,71 @@ function About() {
     const classes = useStyles();
 
     return (
-    <div class="background">
-        <div class= "title">
-            <h1>About Us</h1>
-        </div>
-
-        <div class = "p">
-            <p>
-                At GameRecordKeeper, we make your game record process easier! We are a team of three developers who would like to make a difference in your life! <br></br>
-
-                We created GameRecordKeeper to convenience board gamers. They can search for games they want to play and create tournaments. Our product keeps the result records for them. <br></br><br></br>
-
-                Our names are: 
-            </p>
+        <body>
+        <div class="background">
+            <div class= "title">
+                <h1>About Us</h1>
             </div>
-            <div style={cardStyle}>
-                <Card className={classes.root} variant="outlined" >
-                    <CardContent>
-                        <CardMedia 
-                            className={classes.media}
-                            image={require("./image/blank-profile-picture.png")}
-                            title="Kieran's profile" 
-                        />
+
+            <div class = "p">
+                <p>
+                        At GameRecordKeeper, we make your game record process easier! We are a team of three developers who would like to make a difference in your life! <br></br><br></br>
+
+                    We created GameRecordKeeper to convenience board gamers. You can search for games you want to play and create tournaments! Our product keeps a clear result records for you. <br></br><br></br>
+
+                    Our names are: 
+                </p>
+                </div>
+                <div style={cardStyle}>
+                    <Card className={classes.root} variant="outlined" >
+                        <CardContent>
+                            <CardMedia 
+                                className={classes.media}
+                                image={require("./image/blank-profile-picture.png")}
+                                title="Kieran's profile" 
+                            />
+                            <Typography variant="h5" component="h2">
+                                Kieran
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Stake Holder, Project Manager and Technical Consultant of this project.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+
+                    <Card className={classes.root} variant="outlined" >
+                        <CardContent>
+                            <CardMedia
+                                className={classes.media}
+                                image={require("./image/blank-profile-picture.png")}
+                                title="Connie's profile"
+                            />
+                            <Typography variant="h5" component="h2">
+                                Connie
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Developer, graduate from University of Auckland, majored in Computer Science.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card className={classes.root} variant="outlined" >
+                        <CardContent>
+                            <CardMedia
+                                className={classes.media}
+                                image={require("./image/blank-profile-picture.png")}
+                                title="Tracy's profile"
+                            />
                         <Typography variant="h5" component="h2">
-                            Kieran
+                            Tracy
                         </Typography>
                         <Typography variant="body2" component="p">
-                            Stake Holder, Project Manager and Technical Consultant of this project.
+                            Developer, second year student from MIT, major in Software Development. 
                         </Typography>
                     </CardContent>
-                </Card>
-
-                <Card className={classes.root} variant="outlined" >
-                    <CardContent>
-                        <CardMedia
-                            className={classes.media}
-                            image={require("./image/blank-profile-picture.png")}
-                            title="Connie's profile"
-                        />
-                        <Typography variant="h5" component="h2">
-                            Connie
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Developer, graduate from University of Auckland, majored in Computer Science.
-                        </Typography>
-                    </CardContent>
-                </Card>
-                <Card className={classes.root} variant="outlined" >
-                    <CardContent>
-                        <CardMedia
-                            className={classes.media}
-                            image={require("./image/blank-profile-picture.png")}
-                            title="Tracy's profile"
-                        />
-                    <Typography variant="h5" component="h2">
-                        Tracy
-                    </Typography>
-                    <Typography variant="body2" component="p">
-                        Developer, second year student from MIT, major in Software Development. 
-                    </Typography>
-                </CardContent>
-                </Card>
+                    </Card>
+                </div>
             </div>
-    </div>
+        </body>
 
 )
 }
